a320eb240197d9db31acd1975a01fddc98805d7f
riuson <riuson@bb0b7fd4-029e-11de-8c4e-4bbb75a408b7>
2009-02-25 18:19:49 +0000

    config in separete file again :)

--- update_wallets.php
+++ update_wallets.php
@@ -11,7 +11,7 @@
 <body class='b-page'>
 	<?php
 		include_once "classes/api2.php";
-		include_once "classes/constants.php";
+		include_once "classes/.config.php";
 		include_once "classes/api_accountbalance.php";
 		include_once "classes/page.php";
 		//include_once "classes/user.php";

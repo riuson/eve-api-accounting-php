c52748f473afeeaa0548dbd99905564bb66125aa
riuson <riuson@bb0b7fd4-029e-11de-8c4e-4bbb75a408b7>
2009-03-08 10:55:24 +0000

    test

--- trunk/classes/.config.php
+++ trunk/classes/.config.php
@@ -4,7 +4,7 @@
     //echo($db_host);
     $db_user = "eabase_local";
     $db_pass = "Xwvnaz7XsRsmmEPr";
-    $db_name = "test_ea_db";
+    $db_name = "testdbea";
     $dcapicode = "4r8731tsnb";
 	$rckey = "jkf*(Y*(#%uioHO9H890AH5IOUNUIOH90JHGIOVN IOHFG8OEY89huiniojgerioj ;ojioJHIOTJ(Pui56'4JV9u(*)u#hiojngngIOHJGNi#nignioehOIN3KMNFGDHNFGIO:nik#niogHIOegnKLen GL4INGIO4NGJHNVIOEJ ERG45ERt5%c45R5%5^&j4j&u5JKh guiHruiohtOI4Y8YTH :o ngOIJHGIRJGKLMNKLMoijmopiJ()Jtopi jiopJIOPTJMop4jtyopmkgk;lmkl;fmhk;rjij(_U*()U^(PRJKGp jIO J RGio RJHT(UJ() :TUJTU(jv4otj9 JTO(vji o;IJTYJ VTP?mo9vjPTRMgiojm ropgjropiuT()UpUJ^V(PJITG) JOgpoij(PUT<V(_IUT9IKRUITUPTU (PGJp9weriHG oERHGoiWHGO?WHGIO?hIO HTOU*Hou8 hO:*Y?TEH*G()ut'0U)U(R#PJKLHIO WEHRLE*F eo;ej8iejfsejgposet/p)TUJ(UJTopi4jtw4'J G jGpwj'GP4jw9tjow49ptjuw490TJ:)U(T:)49";
 ?>
